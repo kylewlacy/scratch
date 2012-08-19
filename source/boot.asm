3813f38fddd4c305b6447d17d3d057a17c1ac554
Kyle Lacy <kylewlacy@me.com>
2012-08-19 16:29:15 -0700

    clean up comments and blank lines

--- source/boot.asm
+++ source/boot.asm
@@ -59,40 +59,40 @@ _main:
     mul word[_bpb.rootEntries]    ; Entry size * number of root entries = size of root (in clusters)
     div word[_bpb.bytesPerSector] ; Clusters / sectors per cluster = sectors of root
     xchg ax, cx
-    
+
     ; Compute the location of the FAT
     mov al, byte[_bpb.totalFATs]
     mul word[_bpb.sectorsPerFAT]
     add ax, word[_bpb.reservedSectors]
     mov word[fs.dataSector], ax
     add word[fs.dataSector], cx
-    
+
     ; Read the FAT to [7C00:0200]
     mov bx, 0x0200
     call ReadSectors
     push ax
-    
+
     mov cx, word[_bpb.rootEntries] ; We want to iterate through each entry
     mov di, 0x0200                 ; The first entry will be here (at [7C00:0200])
-  
+
   .findFile:
     push cx
-    mov cx, 0x000b
-    mov si, filename
+    mov cx, 0x000b   ; 11 (0x000b) characters in a filename
+    mov si, filename ; This is the filename we're looking for
     push di
-    rep cmpsb
+    rep cmpsb        ; Compare the filename to the current entry
     pop di
-    je .loadFile
+    je .loadFile     ; We have a match! Load this entry!
     pop cx
-    add di, 0x0020
-    loop .findFile
-    jmp .fail
-  
+    add di, 0x0020   ; Otherwise, go to the next entry,
+    loop .findFile   ; And try again
+    jmp .fail        ; We never found our file
+
   ; DI + 0x001A now contains the file's first cluster
   .loadFile:
     .load.fat:
       mov dx, word[di+0x001a]
-      mov word[fileCluster], dx          ; Save the file's first cluster to file
+      mov word[fileCluster], dx          ; Save the file's first cluster to memory
 
       ; Store FAT size in AX (FAT size = Number of FATs + Sectors per FAT)
       xor ax, ax
@@ -104,13 +104,13 @@ _main:
 
       mov bx, 0x0200
       call ReadSectors                   ; Copy the FAT to 0x0200
-      
+
       ; Load the next stage to [0050:0000]
       mov ax, 0x0050
       mov es, ax
       mov bx, 0x0000
       push bx
-    
+
     .load.file:
       mov ax, word[fileCluster]
       pop bx
@@ -119,7 +119,7 @@ _main:
       mov cl, byte[_bpb.sectorsPerCluster]
       call ReadSectors
       push bx
-      
+
       mov ax, word[fileCluster]
       mov cx, ax
       mov dx, ax
@@ -128,36 +128,24 @@ _main:
       mov bx, 0x0200
       add bx, cx
       mov dx, word[bx]
-      
+
       mov word[fileCluster], dx
       cmp dx, 0xfff8
       jb .load.file
-      
+
       push word 0x0050
       push word 0x0000
       retf
-              
+
       cli
       hlt
       
   .fail:
-  
-    ; ; DEBUG
-    ; push ax
-    ; mov ah, 0eh
-    ; mov al, 'E'
-    ; int 10h
-    ; pop ax
-
     mov si, error
     call Print
 
     cli
     hlt
-    
-    ; mov ah, 00h
-    ; int 16h
-    ; int 19h
 
 %include "lib/base.inc"
 
@@ -173,31 +161,31 @@ ClusterToLBA: ; Based on the conversion equation LBA = ((Cluster - 2) * SectorsP
   mov cl, byte[_bpb.sectorsPerCluster]
   mul cx
   add ax, word[fs.dataSector]
-  
+
   ret
 
 ; LBA to CHS - Convert from an LBA- to CHS- addressing system'
 ; Parameters:
 ;  AX - LBA address to convert
 ; Returns:
-;  [chs.track]
-;  [chs.head]
-;  [chs.sector]
+;  byte[chs.track] - CHS track
+;  byte[chs.head] - CHS head
+;  byte[chs.sector] - CHS sector
 LBAToCHS: 
   ; Based on the equation Sector = (LBA % Sectors per Track) + 1
   xor dx, dx
   div word[_bpb.sectorsPerTrack]        ; Calculate the modulo (in DL)
   inc dl
   mov byte[chs.sector], dl          ; Store it
-  
+
   ; Based on the equation Head = (LBA / Sectors per Track) % Heads per Cylinder
   xor dx, dx
   div WORD [_bpb.headsPerCylinder]        ; AX already contains LBA / Sectors per Track
   mov byte[chs.head], dl
-  
+
   ; Based on the equation Track = LBA / (Sectors per Track * Number of Heads)
   mov byte[chs.track], al           ; Very conveniently-placed AX; it already contains the output!
-  
+
   ret
 
 ; Read Sectors - Reads sectors from disk into memory
@@ -205,55 +193,59 @@ LBAToCHS:
 ;  CX - Number of sectors to read
 ;  AX - Starting sector to read
 ;  ES:BX - Address to read to
+;  byte[chs.track] - Track to read from
+;  byte[chs.sector] - Sector to read from
+;  byte[chs.head] - Head to read from
+;  byte[fs.driveNumber] - Drive to read from
 ; Returns:
 ;  [ES:BX] - Data from disk
 ReadSectors:
-  mov di, 0x0005                ; How many times should we retry the read?
-  
+  mov di, 0x0005                      ; How many times should we retry the read?
+
   ReadSectors.loop:
     push ax
     push bx
     push cx
-    
+
     call LBAToCHS
-    
-    mov ah, 02h                  ; Set the interrupt to the 'read sector' function
-    mov al, 1                    ; Only read one sector
-    mov ch, byte[chs.track]      ; The track to read from
-    mov cl, byte[chs.sector]     ; The sector to read from
-    mov dh, byte[chs.head]       ; The head to read from
-    mov dl, byte[fs.driveNumber] ; The drive to read from
-    int 13h                      ; Call our 'disk IO' interrupt
-    jnc ReadSectors.success      ; If we successfully read the data, we don't have to try again
-    mov ah, 00h                  ; Set the interrupt to the 'reset disk' function
-    int 13h                      ; Call our 'disk IO' interrupt
-    dec di                       ; Decrement our error counter
+
+    mov ah, 02h                       ; Set the interrupt to the 'read sector' function
+    mov al, 1                         ; Only read one sector
+    mov ch, byte[chs.track]           ; The track to read from
+    mov cl, byte[chs.sector]          ; The sector to read from
+    mov dh, byte[chs.head]            ; The head to read from
+    mov dl, byte[fs.driveNumber]      ; The drive to read from
+    int 13h                           ; Call our 'disk IO' interrupt
+    jnc ReadSectors.success           ; If we successfully read the data, we don't have to try again
+    mov ah, 00h                       ; Set the interrupt to the 'reset disk' function
+    int 13h                           ; Call our 'disk IO' interrupt
+    dec di                            ; Decrement our error counter
     pop cx
     pop bx
     pop ax
-    jnz ReadSectors.loop         ; Try again if we've failed
-    jmp ReadSectors.fail         ; RED ALERT
-  
+    jnz ReadSectors.loop              ; Try again if we've failed
+    jmp ReadSectors.fail              ; RED ALERT
+
   ReadSectors.success:
     pop cx
     pop bx
     pop ax
-    
+
     add bx, word[_bpb.bytesPerSector] ; Go to the next memory location
     inc ax                            ; Read from the next sector
     loop ReadSectors
-    
+
     ret
   ReadSectors.fail:
     mov si, error
     call Print
-    
+
     pop cx
     pop bx
     pop ax
-    
-    int 18h ; Call the interrupt indicating a boot failure
+
+    int 18h            ; Call the interrupt indicating a boot failure
     ret
 
-times 510-($-$$) db 0
+times 510-($-$$) db 00 ; We need to fill exactly 512 bytes
 dw 0xaa55
